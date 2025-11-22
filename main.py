6b6891219a53305c1fb9dd47c0c9cd0ef8b7b36a
Jason <jasn74939@gmail.com>
2025-11-22 14:17:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
 from datetime import datetime
 
-path = input("Enter file path: ")
+path = input("enter file path: ")
 
 if os.path.exists(path): 
-    print("File Exists")
+    print("file exists")
 else: 
     print("Incorrect File Path")
     exit()
 
 if os.path.isdir(path):
-    print("This is a folder.")
-    print("Files inside this folder:")
+    print("this is a folder")
+    print("files inside this folder:")
     files = os.listdir(path)
     print(files)
 
 elif os.path.isfile(path):
-    print("This is a file.")
+    print("this is a file")
 
     size = os.path.getsize(path)
-    print("Size:", size, "bytes")
+    print("size:", size, "bytes")
 
     stats = os.stat(path)
     print("Created:", datetime.fromtimestamp(stats.st_ctime))
@@ -28,11 +28,11 @@ elif os.path.isfile(path):
 ext = os.path.splitext(path)[1]
 print("Extension:", ext)
 if ext in [".png", ".jpg", ".jpeg"]:
-    print("Type: Image")
+    print("Image")
 elif ext in [".txt"]:
-    print("Type: Text file")
+    print("Text")
 elif ext in [".mp4", ".mov"]:
-    print("Type: Video")
+    print("Video")
 else:
-    print("Type: Unknown or unsupported")
+    print("unknown or unsupported")
 
